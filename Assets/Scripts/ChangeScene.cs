5398d04d5d67e1285916b271482b71a39aaeab28
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-04-05 22:51:48 -0300

    Implementation of pause in-game

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,9 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public GameObject pause;
+    public GameObject pauseChildren;
+
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+    }
+    public void PauseGame() {
+        Time.timeScale = 0;
+        pause.SetActive(true);
+        pauseChildren.SetActive(true);
+    }
+    public void Resume ()
+    {
+        Time.timeScale = 1;
+        pause.SetActive(false);
+        pauseChildren.SetActive(false);
     }
     public void StartGame()
     {
